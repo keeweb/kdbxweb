dfb082b063006ed52c8dda6a3285519845a5b94b
Strikeskids <github@strikeskids.com>
2016-03-16 21:41:14 -0400

    Migrate key-encryptor to expose promises externally
    This small change keeps coding style consistent by migrating the
    promises returned by webcrypto to the external interface of
    KeyEncryptor.

--- test/crypto/key-encryptor.spec.js
+++ test/crypto/key-encryptor.spec.js
@@ -23,7 +23,7 @@ describe('KeyEncryptor', function() {
     });
 
     it('decrypts one round with default algorithm', function(done) {
-        KeyEncryptor.encrypt(data, key, 1, function(res) {
+        KeyEncryptor.encrypt(data, key, 1).then(function(res) {
             expect(asmCrypto.bytes_to_hex(res)).to.be('46e891c182a31d005a8990ac5d61bb2124ffe5927fa008a739a9b0d217c79717');
             done();
         });
@@ -33,7 +33,7 @@ describe('KeyEncryptor', function() {
         it('uses fallback encryption if webcrypto.importKey generates promise error', function (done) {
             var oldImportKey = subtle.importKey;
             subtle.importKey = function () { return new Promise(function (resolve, reject) { reject('fail'); }); };
-            KeyEncryptor.encrypt(data, key, 1, function (res) {
+            KeyEncryptor.encrypt(data, key, 1).then(function (res) {
                 subtle.importKey = oldImportKey;
                 expect(asmCrypto.bytes_to_hex(res)).to.be('46e891c182a31d005a8990ac5d61bb2124ffe5927fa008a739a9b0d217c79717');
                 done();
@@ -43,7 +43,7 @@ describe('KeyEncryptor', function() {
         it('uses fallback encryption if webcrypto.encrypt generates promise error', function (done) {
             var oldEncrypt = subtle.encrypt;
             subtle.encrypt = function () { return new Promise(function (resolve, reject) { reject('fail'); }); };
-            KeyEncryptor.encrypt(data, key, 1, function (res) {
+            KeyEncryptor.encrypt(data, key, 1).then(function (res) {
                 subtle.encrypt = oldEncrypt;
                 expect(asmCrypto.bytes_to_hex(res)).to.be('46e891c182a31d005a8990ac5d61bb2124ffe5927fa008a739a9b0d217c79717');
                 done();
@@ -53,7 +53,7 @@ describe('KeyEncryptor', function() {
         it('uses fallback encryption if webcrypto.importKey throws an error', function (done) {
             var oldImportKey = subtle.importKey;
             subtle.importKey = function () { throw 'err'; };
-            KeyEncryptor.encrypt(data, key, 1, function (res) {
+            KeyEncryptor.encrypt(data, key, 1).then(function (res) {
                 subtle.importKey = oldImportKey;
                 expect(asmCrypto.bytes_to_hex(res)).to.be('46e891c182a31d005a8990ac5d61bb2124ffe5927fa008a739a9b0d217c79717');
                 done();
@@ -63,7 +63,7 @@ describe('KeyEncryptor', function() {
         it('uses fallback encryption if webcrypto.encrypt throws an error', function (done) {
             var oldImportKey = subtle.encrypt;
             subtle.encrypt = function () { throw 'err'; };
-            KeyEncryptor.encrypt(data, key, 1, function (res) {
+            KeyEncryptor.encrypt(data, key, 1).then(function (res) {
                 subtle.encrypt = oldImportKey;
                 expect(asmCrypto.bytes_to_hex(res)).to.be('46e891c182a31d005a8990ac5d61bb2124ffe5927fa008a739a9b0d217c79717');
                 done();
@@ -79,7 +79,7 @@ describe('KeyEncryptor', function() {
     });
 
     it('decrypts two rounds with default algorithm', function(done) {
-        KeyEncryptor.encrypt(data, key, 2, function(res) {
+        KeyEncryptor.encrypt(data, key, 2).then(function(res) {
             expect(asmCrypto.bytes_to_hex(res)).to.be('1818f732cb1a933911ec90baed252d388980cd3665e1009705e5007aa48ad916');
             done();
         });
@@ -93,7 +93,7 @@ describe('KeyEncryptor', function() {
     });
 
     it('decrypts many rounds with default algorithm', function(done) {
-        KeyEncryptor.encrypt(data, key, 10021, function(res) {
+        KeyEncryptor.encrypt(data, key, 10021).then(function(res) {
             expect(asmCrypto.bytes_to_hex(res)).to.be('64d62f7ec4a363ff0fbb4520163b478ef4d0d631b690a2e7daa6bc09bca092df');
             done();
         });
