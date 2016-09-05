89c934f4eedfd46b0698dc20af1a6b3aa5716d4b
antelle <dmitryvit@gmail.com>
2016-09-05 23:44:00 +0300

    key-encryptor step

--- test/crypto/key-encryptor.spec.js
+++ test/crypto/key-encryptor.spec.js
@@ -1,7 +1,5 @@
 'use strict';
 
-require('../test-support/crypto-mock-node');
-
 var expect = require('expect.js'),
     KeyEncryptor = require('../../lib/crypto/key-encryptor'),
     ByteUtils = require('../../lib/utils/byte-utils');
@@ -10,24 +8,21 @@ describe('KeyEncryptor', function() {
     var data = ByteUtils.hexToBytes('5d18f8a5ae0e7ea86f0ad817f0c0d40656ef1da6367d8a88508b3c13cec0d7af');
     var key = ByteUtils.hexToBytes('ee66af917de0b0336e659fe6bd40a337d04e3c2b3635210fa16f28fb24d563ac');
 
-    it('decrypts one round', function(done) {
-        KeyEncryptor.encrypt(data, key, 1, function(res) {
+    it('decrypts one round', function() {
+        return KeyEncryptor.encrypt(data, key, 1).then(function(res) {
             expect(ByteUtils.bytesToHex(res)).to.be('46e891c182a31d005a8990ac5d61bb2124ffe5927fa008a739a9b0d217c79717');
-            done();
         });
     });
 
-    it('decrypts two rounds', function(done) {
-        KeyEncryptor.encrypt(data, key, 2, function(res) {
+    it('decrypts two rounds', function() {
+        return KeyEncryptor.encrypt(data, key, 2).then(function(res) {
             expect(ByteUtils.bytesToHex(res)).to.be('1818f732cb1a933911ec90baed252d388980cd3665e1009705e5007aa48ad916');
-            done();
         });
     });
 
-    it('decrypts many rounds', function(done) {
-        KeyEncryptor.encrypt(data, key, 10021, function(res) {
+    it('decrypts many rounds', function() {
+        return KeyEncryptor.encrypt(data, key, 10021).then(function(res) {
             expect(ByteUtils.bytesToHex(res)).to.be('64d62f7ec4a363ff0fbb4520163b478ef4d0d631b690a2e7daa6bc09bca092df');
-            done();
         });
     });
 });
