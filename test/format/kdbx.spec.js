e3adbe00394bcb57136cffaca7932748f0268d86
antelle <dmitryvit@gmail.com>
2016-09-10 21:51:00 +0300

    update argon2 db

--- test/format/kdbx.spec.js
+++ test/format/kdbx.spec.js
@@ -107,7 +107,7 @@ describe('Kdbx', function () {
     });
 
     xit('loads kdbx4 file with argon2 kdf', function() {
-        var cred = new kdbxweb.Credentials(kdbxweb.ProtectedValue.fromString('test'));
+        var cred = new kdbxweb.Credentials(kdbxweb.ProtectedValue.fromString('demo'));
         return kdbxweb.Kdbx.load(TestResources.argon2, cred).then(function(db) {
             expect(db).to.be.a(kdbxweb.Kdbx);
             // return db.save().then(function(ab) {
