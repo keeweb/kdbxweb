64a64d54d7262bd2cf42cf8841435684050438f7
antelle <dmitryvit@gmail.com>
2020-12-07 21:12:45 +0100

    refactored resource loading

--- test/test-support/test-resources.js
+++ test/test-support/test-resources.js
@@ -2,187 +2,47 @@
 
 var ByteUtils = require('../../lib/utils/byte-utils');
 
-var res = {};
+var res = {
+    demoKdbx: readFile('demo.kdbx'),
+    demoKey: readFile('demo.key'),
+    demoXml: readFile('demo.xml'),
+    cyrillicKdbx: readFile('cyrillic.kdbx'),
+    binKeyKdbx: readFile('binkey.kdbx'),
+    binKeyKey: readFile('binkey.key'),
+    emptyPass: readFile('EmptyPass.kdbx'),
+    emptyPassWithKeyFile: readFile('EmptyPassWithKeyFile.kdbx'),
+    emptyPassWithKeyFileKey: readFile('EmptyPassWithKeyFile.key'),
+    noPassWithKeyFile: readFile('NoPassWithKeyFile.kdbx'),
+    noPassWithKeyFileKey: readFile('NoPassWithKeyFile.key'),
+    key32: readFile('Key32.kdbx'),
+    key32KeyFile: readFile('Key32.key'),
+    key64: readFile('Key64.kdbx'),
+    key64KeyFile: readFile('Key64.key'),
+    keyWithBom: readFile('KeyWithBom.kdbx'),
+    keyWithBomKeyFile: readFile('KeyWithBom.key'),
+    keyV2: readFile('KeyV2.kdbx'),
+    keyV2KeyFile: readFile('KeyV2.keyx'),
+    badVersionKeyFile: readFile('KeyBadVersion.keyx'),
+    badHashV2KeyFile: readFile('KeyBadHashV2.keyx'),
+    argon2: readFile('Argon2.kdbx'),
+    argon2id: readFile('Argon2id.kdbx'),
+    argon2ChaCha: readFile('Argon2ChaCha.kdbx'),
+    aesChaCha: readFile('AesChaCha.kdbx'),
+    aesKdfKdbx4: readFile('AesKdfKdbx4.kdbx'),
+    yubikey3: readFile('YubiKey3.kdbx'),
+    yubikey4: readFile('YubiKey4.kdbx'),
+    emptyUuidXml: readFile('empty-uuid.xml')
+};
 
-try {
-    res.demoKdbx = require('base64-loader!../../resources/demo.kdbx');
-} catch (e) {
-    res.demoKdbx = readNodeFile('../../resources/demo.kdbx');
-}
-
-try {
-    res.demoKey = require('base64-loader!../../resources/demo.key');
-} catch (e) {
-    res.demoKey = readNodeFile('../../resources/demo.key');
-}
-
-try {
-    res.demoXml = require('base64-loader!../../resources/demo.xml');
-} catch (e) {
-    res.demoXml = readNodeFile('../../resources/demo.xml');
-}
-
-try {
-    res.cyrillicKdbx = require('base64-loader!../../resources/cyrillic.kdbx');
-} catch (e) {
-    res.cyrillicKdbx = readNodeFile('../../resources/cyrillic.kdbx');
-}
-
-try {
-    res.binKeyKdbx = require('base64-loader!../../resources/binkey.kdbx');
-} catch (e) {
-    res.binKeyKdbx = readNodeFile('../../resources/binkey.kdbx');
-}
-
-try {
-    res.binKeyKey = require('base64-loader!../../resources/binkey.key');
-} catch (e) {
-    res.binKeyKey = readNodeFile('../../resources/binkey.key');
-}
-
-try {
-    res.emptyPass = require('base64-loader!../../resources/EmptyPass.kdbx');
-} catch (e) {
-    res.emptyPass = readNodeFile('../../resources/EmptyPass.kdbx');
-}
-
-try {
-    res.emptyPassWithKeyFile = require('base64-loader!../../resources/EmptyPassWithKeyFile.kdbx');
-} catch (e) {
-    res.emptyPassWithKeyFile = readNodeFile('../../resources/EmptyPassWithKeyFile.kdbx');
-}
-
-try {
-    res.emptyPassWithKeyFileKey = require('base64-loader!../../resources/EmptyPassWithKeyFile.key');
-} catch (e) {
-    res.emptyPassWithKeyFileKey = readNodeFile('../../resources/EmptyPassWithKeyFile.key');
-}
-
-try {
-    res.noPassWithKeyFile = require('base64-loader!../../resources/NoPassWithKeyFile.kdbx');
-} catch (e) {
-    res.noPassWithKeyFile = readNodeFile('../../resources/NoPassWithKeyFile.kdbx');
-}
-
-try {
-    res.noPassWithKeyFileKey = require('base64-loader!../../resources/NoPassWithKeyFile.key');
-} catch (e) {
-    res.noPassWithKeyFileKey = readNodeFile('../../resources/NoPassWithKeyFile.key');
-}
-
-try {
-    res.key32 = require('base64-loader!../../resources/Key32.kdbx');
-} catch (e) {
-    res.key32 = readNodeFile('../../resources/Key32.kdbx');
-}
-
-try {
-    res.key32KeyFile = require('base64-loader!../../resources/Key32.key');
-} catch (e) {
-    res.key32KeyFile = readNodeFile('../../resources/Key32.key');
-}
-
-try {
-    res.key64 = require('base64-loader!../../resources/Key64.kdbx');
-} catch (e) {
-    res.key64 = readNodeFile('../../resources/Key64.kdbx');
-}
-
-try {
-    res.key64KeyFile = require('base64-loader!../../resources/Key64.key');
-} catch (e) {
-    res.key64KeyFile = readNodeFile('../../resources/Key64.key');
-}
-
-try {
-    res.keyWithBom = require('base64-loader!../../resources/KeyWithBom.kdbx');
-} catch (e) {
-    res.keyWithBom = readNodeFile('../../resources/KeyWithBom.kdbx');
-}
-
-try {
-    res.keyWithBomKeyFile = require('base64-loader!../../resources/KeyWithBom.key');
-} catch (e) {
-    res.keyWithBomKeyFile = readNodeFile('../../resources/KeyWithBom.key');
-}
-
-try {
-    res.keyV2 = require('base64-loader!../../resources/KeyV2.kdbx');
-} catch (e) {
-    res.keyV2 = readNodeFile('../../resources/KeyV2.kdbx');
-}
-
-try {
-    res.keyV2KeyFile = require('base64-loader!../../resources/KeyV2.keyx');
-} catch (e) {
-    res.keyV2KeyFile = readNodeFile('../../resources/KeyV2.keyx');
-}
-
-try {
-    res.badVersionKeyFile = require('base64-loader!../../resources/KeyBadVersion.keyx');
-} catch (e) {
-    res.badVersionKeyFile = readNodeFile('../../resources/KeyBadVersion.keyx');
-}
-
-try {
-    res.badHashV2KeyFile = require('base64-loader!../../resources/KeyBadHashV2.keyx');
-} catch (e) {
-    res.badHashV2KeyFile = readNodeFile('../../resources/KeyBadHashV2.keyx');
-}
-
-try {
-    res.argon2 = require('base64-loader!../../resources/Argon2.kdbx');
-} catch (e) {
-    res.argon2 = readNodeFile('../../resources/Argon2.kdbx');
-}
-
-try {
-    res.argon2id = require('base64-loader!../../resources/Argon2id.kdbx');
-} catch (e) {
-    res.argon2id = readNodeFile('../../resources/Argon2id.kdbx');
-}
-
-try {
-    res.argon2ChaCha = require('base64-loader!../../resources/Argon2ChaCha.kdbx');
-} catch (e) {
-    res.argon2ChaCha = readNodeFile('../../resources/Argon2ChaCha.kdbx');
-}
-
-try {
-    res.aesChaCha = require('base64-loader!../../resources/AesChaCha.kdbx');
-} catch (e) {
-    res.aesChaCha = readNodeFile('../../resources/AesChaCha.kdbx');
-}
-
-try {
-    res.aesKdfKdbx4 = require('base64-loader!../../resources/AesKdfKdbx4.kdbx');
-} catch (e) {
-    res.aesKdfKdbx4 = readNodeFile('../../resources/AesKdfKdbx4.kdbx');
-}
-
-try {
-    res.yubikey3 = require('base64-loader!../../resources/YubiKey3.kdbx');
-} catch (e) {
-    res.yubikey3 = readNodeFile('../../resources/YubiKey3.kdbx');
-}
-
-try {
-    res.yubikey4 = require('base64-loader!../../resources/YubiKey4.kdbx');
-} catch (e) {
-    res.yubikey4 = readNodeFile('../../resources/YubiKey4.kdbx');
-}
-
-try {
-    res.emptyUuidXml = require('base64-loader!../../resources/empty-uuid.xml');
-} catch (e) {
-    res.emptyUuidXml = readNodeFile('../../resources/empty-uuid.xml');
-}
-
-for (var file in res) {
-    if (res[file]) {
-        var content = res[file];
-        res[file] = ByteUtils.arrayToBuffer(ByteUtils.base64ToBytes(content));
+function readFile(name) {
+    let content;
+    try {
+        content = require('base64-loader!../../resources/' + name);
+    } catch (e) {
+        content = readNodeFile('../../resources/' + name);
     }
+    content = ByteUtils.arrayToBuffer(ByteUtils.base64ToBytes(content));
+    return content;
 }
 
 function readNodeFile(filePath) {
