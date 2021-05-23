1d4ccd093123ccf8cc6dacbc3664fa6b0e099139
antelle <dmitryvit@gmail.com>
2021-05-23 23:17:13 +0200

    option to generate an importable xml

--- scripts/kdbx-to-xml.ts
+++ scripts/kdbx-to-xml.ts
@@ -6,24 +6,42 @@ CryptoEngine.setArgon2Impl(argon2);
 
 if (process.argv.length < 4) {
     console.log('Usage: npm run script:kdbx-to-xml path/to-file.kdbx password');
+    console.log('To make an XML that can be imported, add "-- --importable" after password');
     process.exit(1);
 }
 
 const filePath = process.argv[2];
 const password = process.argv[3];
+const importable = process.argv.includes('--importable');
 const file = new Uint8Array(fs.readFileSync(filePath)).buffer;
 const cred = new Credentials(ProtectedValue.fromString(password));
 
-Kdbx.load(file, cred, { preserveXml: true })
-    .then((db) => {
-        if (!db.xml) {
-            throw new Error('XML not read');
-        }
-        const xml = XmlUtils.serialize(db.xml);
-        fs.writeFileSync(filePath + '.xml', xml);
-        console.log('Done, written', filePath + '.xml');
-    })
-    .catch((e) => {
-        console.error('Error', e);
-        process.exit(2);
-    });
+(async () => {
+    const db = await Kdbx.load(file, cred, { preserveXml: true });
+    if (!db.xml) {
+        throw new Error('XML not read');
+    }
+    let xml: string;
+    if (importable) {
+        xml = await db.saveXml(true);
+    } else {
+        xml = XmlUtils.serialize(db.xml);
+    }
+    fs.writeFileSync(filePath + '.xml', xml);
+    console.log('Done, written', filePath + '.xml');
+    console.log(
+        "WARNING: the XML contains raw passwords as well as other data, don't paste it anywhere!"
+    );
+    if (importable) {
+        console.log('This XML can be imported in applications compatible with KeePass.');
+    } else {
+        console.log(
+            "The generated XML is a raw XML from your database, if you import it, passwords won't match. " +
+                'If you would like to generate an XML file suitable for import, add "-- --importable" after your password: ' +
+                'npm run script:kdbx-to-xml path/to-file.kdbx password -- --importable'
+        );
+    }
+})().catch((e) => {
+    console.error('Error', e);
+    process.exit(2);
+});
